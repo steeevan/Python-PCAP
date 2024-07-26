b7167cc2108a2c9d7d6d7c8e425b819a5f06aca3
Brista Tan <bristatan@Bristas-MacBook-Air.local>
2024-07-26 13:05:33 -0700

    This is my homework 2-1 files added.

---
+++ Homework/myHomework/Homework2 files/challenge#16.py
@@ -0,0 +1,10 @@
+# Create the list
+squares = [1,4,9,16,25,36,49,64,81,100]
+# print the first one
+print(squares[0])
+# print the fourth one
+print(squares[3])
+# print the last one
+print(squares[-1])
+# print the fourth-to-last one
+print(squares[-4])
